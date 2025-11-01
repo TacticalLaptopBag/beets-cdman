345591c8a56d035ad3a0febcb8ac7a317f9b2a78
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2025-11-01 01:29:01 -0400

    Add from_str

--- beetsplug/cd/audio/audio_populate_mode.py
+++ beetsplug/cd/audio/audio_populate_mode.py
@@ -5,3 +5,15 @@ class AudioPopulateMode(Enum):
     SOFT_LINK = "soft_link"
     HARD_LINK = "hard_link"
     COPY = "copy"
+
+    @classmethod
+    def from_str(cls, string: str):
+        match string.lower():
+            case "soft_link":
+                return cls.SOFT_LINK
+            case "hard_link":
+                return cls.HARD_LINK
+            case "copy":
+                return cls.COPY
+            case _:
+                return None
